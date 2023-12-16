25c1003fb240da5d68d005254dff450af9a3c575
PeiCheng0413 <32326744+PeiCheng0413@users.noreply.github.com>
2023-12-16 12:35:03 +0800

    Add tarot example

---
+++ Tarot/tarot.go
@@ -0,0 +1,14 @@
+package main
+
+type tarot []string
+
+func newTarot()tarot{
+	tarotSuits := []string{"金幣","權杖","聖杯","寶劍"}
+	tarotValues := []string{"王牌","一","二","三","四","五","六","七","八","九","十"}
+	var t tarot
+	for _,suit:= range tarotSuits{
+		for_,value:= range tarotValues{
+			t
+		}
+	}
+}
