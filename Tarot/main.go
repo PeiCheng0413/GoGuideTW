6a20e1d916d7d0dff9fa67ccfb789e1021eebe67
PeiCheng0413 <32326744+PeiCheng0413@users.noreply.github.com>
2023-12-17 06:49:53 +0800

    update stuff again

--- Tarot/main.go
+++ Tarot/main.go
@@ -3,4 +3,7 @@ package main
 func main() {
 	ta := newTarot()
 	ta.print()
+	ta.shuffle()
+	tadraw, _ := draw(ta, 4)
+	tadraw.print()
 }
--- Tarot/tarot.go
+++ Tarot/tarot.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
 
 type tarot []string
 
@@ -19,5 +25,39 @@ func newTarot() tarot {
 func (ta tarot) print() {
 	for i, t := range ta {
 		fmt.Println(i, t)
+	}
+}
+
+func draw(t tarot, num int) (tarot, tarot) {
+	return t[:num], t[num:]
+}
+
+func (t tarot) toString() string {
+	return strings.Join([]string(t), ",")
+}
+
+func (t tarot) saveToFile(filename string) error {
+	return os.WriteFile(filename, []byte(t.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) tarot {
+	bs, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return tarot(s)
+}
+
+func (t tarot) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range t {
+		newPosition := r.Intn(len(t) - 1)
+
+		t[i], t[newPosition] = t[newPosition], t[i]
 	}
 }
